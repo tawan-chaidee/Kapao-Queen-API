baaf402df67cfb2e20ab4bd83fc515d8494a4a77
pastelite <kana.bhong@gmail.com>
2023-04-25 00:26:45 +0700

    better search

--- index.js
+++ index.js
@@ -78,30 +78,80 @@ app.get('/itemSearch', function (req,res){
     })
 })
 
+// My idea
+// search by name, tag, description, min/max price
 app.get('/itemAdvanceSearch', function (req,res){
-    let id = req.query.id;
-    let name = req.query.name;
-    let tag = req.query.tag;
 
-    let sql = `
-    select *
-    from foodlist
-    where id like '%${id}%'
-    or tag1 like '%${tag}%' 
-    or tag2 like '%${tag}%'
-    or name like '%${name}%';
-    `
+    console.log(req.query)
+
+    let query = "SELECT * FROM foodlist"
+    let whereClause = []
+    let andClause = []
+
+    if (req.query.name) {
+        whereClause.push(`name LIKE '%${req.query.name}%'`)
+    }
+
+    if (req.query.tag) {
+        whereClause.push(`taglist LIKE '%${req.query.tag}%'`)
+    }
+
+    if (req.query.description) {
+        whereClause.push(`description LIKE '%${req.query.description}%'`)
+    }
+
+    if (req.query.minPrice) {
+        andClause.push(`price >= ${req.query.minPrice}`)
+    }
+
+    if (req.query.maxPrice) {
+        andClause.push(`price <= ${req.query.maxPrice}`)
+    }
+
+    if (whereClause.length > 0) {
+        query += " WHERE " + whereClause.join(" AND ")
+    }
+
+    if (andClause.length > 0) {
+        if (whereClause.length === 0) {
+            query += " WHERE "
+        } else {
+            query += " AND "
+        }
+        query += andClause.join(" AND ")
+    }
+
+    console.log(query)
 
 
-    connection.query(sql, function (error, results) {
+
+    // let id = req.query.id;
+    // let name = req.query.name;
+    // let tag = req.query.tag;
+
+    // let sql = `
+    // select *
+    // from foodlist
+    // where id like '%${id}%'
+    // or taglist like '%${tag}%' 
+    // or name like '%${name}%';
+    // `
+
+    // connection.query("SELECT * FROM foodlist WHERE id L")
+
+
+    connection.query(query, function (error, result) {
         if (error) {
             console.error(error);
             return res.status(500).send({
-                error: true,
+                success: false,
                 message: 'Internal server error',
             });
         }
-        res.json(results);
+        res.json({
+            success: true,
+            result
+        });
     })
 })
 
@@ -114,12 +164,12 @@ app.post('/itemSubmit', function (req, res) {
     if (error) {
         console.error(error);
         return res.status(500).send({
-            error: true,
+            success: false,
             message: 'Invalid Input',
         });
     }
     return res.send({
-        error: false,
+        success: true,
         data: results.affectedRows,
         message: 'New item has been created successfully.',
     });
@@ -146,11 +196,21 @@ app.get('/itemDetail', function (req, res) {
         }
 
         // replace taglist
-        let splittedTags = rows[0].taglist.split(',');
+        let splittedTags = rows[0].taglist?.split(',');
         rows[0].taglist = splittedTags;
 
         // get ingredients
-        let [ingredients,_] = await connection.promise().query(`SELECT name, img from food_ingredients fi left join ingredients i on fi.ingredient_id = i.id where fi.food_id = ${id}`)
+        // the ingredient is Name|Link,Name|Link
+        let ingredients = rows[0].ingredientslist
+            .split(',')
+            .map(ingredient => {
+                let splittedIngredient = ingredient.split('|');
+                return {
+                    name: splittedIngredient[0],
+                    img: splittedIngredient[1]
+                }
+            });
+        rows[0].ingredientslist = ingredients;
 
         res.json({
             success: true,
@@ -188,20 +248,6 @@ app.put('/itemUpdate', function (req, res) {
     });
 });
 
-app.get('/ingredientList', function (req, res) {
-    const query = 'SELECT * FROM ingredients';
-    connection.query(query, function (err, rows, fields) {
-        if (err) {
-            console.error('Error querying database: ' + err.stack);
-            return res.status(500).send('Internal server error');
-        }
-        res.send({
-            success: true,
-            result: rows
-        })
-    });
-});
-
 app.delete('/itemDelete', function (req, res) {
 
     console.log(req.query)
@@ -215,11 +261,11 @@ app.delete('/itemDelete', function (req, res) {
         if (error) {
             console.error(error);
             return res.status(500).send({
-                error: true,
+                success: false,
                 message: 'Invalid Input',
             });
         }
-        return res.send({ error: false, data: results.affectedRows, message: 'item has been deleted successfully.' });
+        return res.send({ success: true, data: results.affectedRows, message: 'item has been deleted successfully.' });
     });
 });
 
